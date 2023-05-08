68e45b3210a3535ebef59c06daed4f8e097c33d1
JEONG SUNWOO <sunwoojeong0430@gmail.com>
2023-05-08 15:55:44 +0900

    Complete ch09

--- Python_Basic_ch09_kiosk/kiosk_fnc.py
+++ Python_Basic_ch09_kiosk/kiosk_fnc.py
@@ -2,15 +2,15 @@
 
 # 1.세부 메뉴 출력
 def print_sub_menu(sub_name, sub_price):
-    for key, value in burger_name.items():
-        print(f"□ {key}.{value}({burger_price[key]}원)")
+    for key, value in sub_name.items():
+        print(f"□ {key}.{value}({sub_price[key]}원)")
 
 
 # 2.사용자 메뉴 선택
 def choice_menu_num(max_num):
     while True:
         menu_num = int(input(">> 번호: "))
-        if menu_num>= 1 and menu_num <= max_num:
+        if menu_num >= 1 and menu_num <= max_num:
             break
         else:
             print(f"경고: 1~{max_num} 사이의 값만 입력해주세요.")
